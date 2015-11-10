a4bb37818e2af107c9fe22079ff75654f201e695
Mando <boadavel@rohan.sdsu.edu>
2015-11-10 15:29:26 -0800

    Added Parameters class

---
+++ include/gam_parameters.h
@@ -0,0 +1,21 @@
+#ifndef GAM_INCLUDE_PARAMETERS_H_
+#define GAM_INCLUDE_PARAMETERS_H_
+
+namespace gam {
+  
+  class Parameters {
+    private:
+      static unsigned int popSize;
+      static unsigned int maxGenerations;
+      static double generationalGap;
+      static double mutationProb;
+      static double bitMutationProb;
+    
+    public:
+      static void initByDefault();
+      static void initByFile();
+  }
+  
+}
+
+#endif // GAM_INCLUDE_PARAMETERS_H_
