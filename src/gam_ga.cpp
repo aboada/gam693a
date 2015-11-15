c8480a42bd0ba9245ef61d42952903740cc3389b
sumukh <sagar.sumukh@gmail.com>
2015-11-15 12:04:47 -0800

    added definition for offspring, evolve, initialize and evalPopulation.

--- src/gam_ga.cpp
+++ src/gam_ga.cpp
@@ -12,8 +12,24 @@ gam::GeneticAlgorithm::GeneticAlgorithm(Parameters p) {
   param = p;
 }
 
+void gam::GeneticAlgorithm::initialize(boolean isInitByFile){
+  param = new Parameters(isInitByFile);
+}
+  
 FitnessType gam::GeneticAlgorithm::evolve() {
   // TODO: implement this method
+  int i;
+  Population pCnt,pNxt,pTmp;
+  pCnt = //I think we need a getPopulation() method here.
+  setParameters(param);
+  while(i<param.getMaxGenerations){
+    pTmp = selection(pCnt);
+    pTmp = offspring(pTmp);
+    evalPopulation(pTmp);
+    pNxt = replacement(pCnt,pTmp);
+  }
+  pNxt.sort();
+    
 }
 
 void gam::GeneticAlgorithm::setParameters(Parameters p) {
@@ -30,6 +46,20 @@ Population gam::GeneticAlgorithm::selection(Population p) {
 
 Population gam::GeneticAlgorithm::offspring(Population p) {
   // TODO: implement this method
+  for (unsigned int i = 0; i < p.popSize; ++i) {
+    int j = Utility.getRandom32(0,1);
+    if(j<param.getMutationProb){
+      p.members[i] = mutate(p.members[i]);
+    }
+    else{
+      int k = Utility.getRandom32(0,p.popSize-1);
+      k = ceil(k);
+      while(k!=i){
+        k = Utility.getRandom32(0,p.popSize-1);
+        k = ceil(k);
+    }
+    p.member[i] = recombination(p.members[i],p.member[k]);
+  }
 }
 
 Individual gam::GeneticAlgorithm::recombination(Individual i1, 
@@ -43,6 +73,7 @@ Individual gam::GeneticAlgorithm::mutate(Individual i) {
 
 void gam::GeneticAlgorithm::evalPopulation(Population p) {
   // TODO: implement this method
+  p.evaluate();
 }
 
 Population gam::GeneticAlgorithm::replacement(Population pCnt, 
